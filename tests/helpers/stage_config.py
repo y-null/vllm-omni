d07927d78fca9b2a8792c0217c017885ba0c9b35
y_null <y_null@qq.com>
2026-07-21 23:05:56 +0800

    chore: fix BOM and trailing whitespace in stage_config.py
    Signed-off-by: y_null <y_null@qq.com>

--- tests/helpers/stage_config.py
+++ tests/helpers/stage_config.py
@@ -604,7 +604,6 @@ _CI_OVERLAYS: dict[str, dict[str, Any]] = {
     },
     "minicpmo_4_5": {
         "pipeline": "minicpmo_4_5",
-
         "base_config": "minicpmo_4_5.yaml",
         "async_chunk": False,
         "stages": [
